e942755f0179fb2fa2d41283992dbc5301f1057d
Deepak kumar <kumardeepak46263@gmail.com>
2026-03-25 15:36:32 +0530

    your message

--- routes/movie.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as movieController from "../controllers/movie.controller.js";
-
-
-const router = Router();
-
-router.post("/mba/api/v1/movies", movieController.createMovie)
-
-export default router;
